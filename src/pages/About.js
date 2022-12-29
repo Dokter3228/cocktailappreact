36e542f41c6e4c55644d0d32a7d6127c6842a2f4
Dmitrii <Dokter322@yandex.ru>
2022-12-29 20:48:03 +0300

    done

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi veritatis
+        maxime, eos numquam non doloribus dolor blanditiis commodi inventore
+        omnis aut sint assumenda vero harum, delectus fuga fugit hic id
+        similique? Sapiente quam quidem soluta reprehenderit ea, officiis iste
+        perspiciatis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
